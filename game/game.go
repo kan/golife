99c72465275c47aa077491c62f61ef8cb900fd81
Kan Fushihara <kan.fushihara@gmail.com>
2024-11-19 21:34:31 +0900

    ゲームの盤面処理をgame.goに分離
    - 設定値をGame構造体に寄せる

---
+++ game/game.go
@@ -0,0 +1,135 @@
+package game
+
+import "sync"
+
+type Game struct {
+	world  GameBoard
+	shadow GameBoard
+	run    bool
+	size   int
+	mu     sync.Mutex
+}
+
+type GameBoard [][]uint
+
+func NewGame(size int) *Game {
+	world := make([][]uint, size)
+	shadow := make([][]uint, size)
+	for i := 0; i < size; i++ {
+		world[i] = make([]uint, size)
+		shadow[i] = make([]uint, size)
+	}
+
+	return &Game{
+		world:  world,
+		shadow: shadow,
+		run:    false,
+		size:   size,
+	}
+}
+
+func (g *Game) StartStop() {
+	g.run = !g.run
+}
+
+func (g *Game) Run() bool {
+	return g.run
+}
+
+func (g *Game) Size() int {
+	return g.size
+}
+
+func (g *Game) GetWorld(x, y int) bool {
+	return g.world[x][y] == 1
+}
+
+func (g *Game) Toggle(x, y int) {
+	if x >= g.size || y >= g.size {
+		return
+	}
+
+	if g.GetWorld(x, y) {
+		g.world[x][y] = 0
+	} else {
+		g.world[x][y] = 1
+	}
+}
+
+func (g *Game) ApplyBoard(w int, f func(x, y int)) {
+	var wg sync.WaitGroup
+	jobs := make(chan [2]int, g.size*g.size)
+
+	for i := 0; i < w; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for job := range jobs {
+				f(job[0], job[1])
+			}
+		}()
+	}
+
+	for y := 0; y < g.size; y++ {
+		for x := 0; x < g.size; x++ {
+			jobs <- [2]int{x, y}
+		}
+	}
+	close(jobs)
+
+	wg.Wait()
+}
+
+func (g *Game) Step(w int) {
+	for y := 0; y < g.size; y++ {
+		for x := 0; x < g.size; x++ {
+			g.shadow[x][y] = 0
+		}
+	}
+
+	g.ApplyBoard(w, func(x, y int) {
+		g.stepBoard(x, y)
+	})
+
+	g.mu.Lock()
+	g.world, g.shadow = g.shadow, g.world
+	g.mu.Unlock()
+}
+
+func (g *Game) stepBoard(x, y int) {
+	sw := uint(0)
+	if x-1 >= 0 && y-1 >= 0 {
+		sw += g.world[x-1][y-1]
+	}
+	if y-1 >= 0 {
+		sw += g.world[x][y-1]
+	}
+	if x+1 < g.size && y-1 >= 0 {
+		sw += g.world[x+1][y-1]
+	}
+	if x-1 >= 0 {
+		sw += g.world[x-1][y]
+	}
+	if x+1 < g.size {
+		sw += g.world[x+1][y]
+	}
+	if x-1 >= 0 && y+1 < g.size {
+		sw += g.world[x-1][y+1]
+	}
+	if y+1 < g.size {
+		sw += g.world[x][y+1]
+	}
+	if x+1 < g.size && y+1 < g.size {
+		sw += g.world[x+1][y+1]
+	}
+
+	if sw < 2 {
+		g.shadow[x][y] = 0
+	} else if sw == 3 {
+		g.shadow[x][y] = 1
+	} else if sw > 3 {
+		g.shadow[x][y] = 0
+	} else {
+		g.shadow[x][y] = g.world[x][y]
+	}
+}
--- main.go
+++ main.go
@@ -2,28 +2,14 @@ package main
 
 import (
 	"runtime"
-	"sync"
 	"time"
 
 	"github.com/gdamore/tcell/v2"
+	"github.com/kan/golife/game"
 )
 
-const SIZE = 20
-
-var world = [][]uint{}
-var shadowWorld = [][]uint{}
-var run = false
+var lifeGame *game.Game
 var interval = 500 * time.Millisecond
-
-func genWorld() [][]uint {
-	w := [][]uint{}
-	for i := 0; i < SIZE; i++ {
-		row := make([]uint, SIZE)
-		w = append(w, row)
-	}
-
-	return w
-}
 
 func main() {
 	screen, err := tcell.NewScreen()
@@ -41,8 +27,7 @@ func main() {
 	screen.EnableMouse()
 	defer screen.DisableMouse()
 
-	world = genWorld()
-	shadowWorld = genWorld()
+	lifeGame = game.NewGame(20)
 
 	eventLoop(screen, w)
 }
@@ -67,7 +52,7 @@ func eventLoop(screen tcell.Screen, w int) {
 				if e.Key() == tcell.KeyEscape {
 					return
 				} else if e.Key() == tcell.KeyCtrlS {
-					run = !run
+					lifeGame.StartStop()
 				} else if e.Rune() == '+' {
 					if interval > 100*time.Millisecond {
 						interval -= 100 * time.Millisecond
@@ -81,16 +66,8 @@ func eventLoop(screen tcell.Screen, w int) {
 				x, y := e.Position()
 				btn := e.Buttons()
 
-				if int(x/2) >= SIZE || y >= SIZE {
-					continue
-				}
-
 				if btn&tcell.Button1 != 0 {
-					if world[int(x/2)][y] == 0 {
-						world[int(x/2)][y] = 1
-					} else {
-						world[int(x/2)][y] = 0
-					}
+					lifeGame.Toggle(int(x/2), y)
 				}
 				showWorld(screen, w)
 			case *tcell.EventResize:
@@ -98,114 +75,27 @@ func eventLoop(screen tcell.Screen, w int) {
 			}
 
 		case <-ticker.C:
-			if run {
-				lifeWorld(w)
+			if lifeGame.Run() {
+				lifeGame.Step(w)
 			}
 			showWorld(screen, w)
 		}
 	}
 }
 
-func showWorld(screen tcell.Screen, w int) {
-	screen.Clear()
-
-	applyWorld(screen, w, dispCell)
-
-	screen.Show()
-}
-
-func lifeWorld(w int) {
-	for y := 0; y < SIZE; y++ {
-		for x := 0; x < SIZE; x++ {
-			shadowWorld[x][y] = 0
-		}
-	}
-	applyWorld(nil, w, lifeCell)
-
-	world, shadowWorld = shadowWorld, world
-}
-
-func applyWorld(screen tcell.Screen, w int, f func(tcell.Screen, int, int)) {
-	var wg sync.WaitGroup
-	jobs := make(chan [2]int, SIZE*SIZE)
-
-	for i := 0; i < w; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for job := range jobs {
-				f(screen, job[0], job[1])
-			}
-		}()
-	}
-
-	for y, row := range world {
-		for x := range row {
-			jobs <- [2]int{x, y}
-		}
-	}
-	close(jobs)
-
-	wg.Wait()
-}
-
 var gs = tcell.StyleDefault.Foreground(tcell.ColorGreen)
 var bs = tcell.StyleDefault.Foreground(tcell.ColorBlack)
 
-func dispCell(screen tcell.Screen, x, y int) {
-	if world[x][y] == 0 {
-		screen.SetContent(x*2, y, rune('■'), nil, bs)
-	} else {
-		screen.SetContent(x*2, y, rune('■'), nil, gs)
-	}
-}
+func showWorld(screen tcell.Screen, w int) {
+	screen.Clear()
 
-/*
-セルの生死は次のルールに従う。
+	lifeGame.ApplyBoard(w, func(x, y int) {
+		if lifeGame.GetWorld(x, y) {
+			screen.SetContent(x*2, y, rune('■'), nil, gs)
+		} else {
+			screen.SetContent(x*2, y, rune('■'), nil, bs)
+		}
+	})
 
-誕生
-死んでいるセルに隣接する生きたセルがちょうど3つあれば、次の世代が誕生する。
-生存
-生きているセルに隣接する生きたセルが2つか3つならば、次の世代でも生存する。
-過疎
-生きているセルに隣接する生きたセルが1つ以下ならば、過疎により死滅する。
-過密
-生きているセルに隣接する生きたセルが4つ以上ならば、過密により死滅する。
-*/
-func lifeCell(screen tcell.Screen, x, y int) {
-	sw := uint(0)
-	if x-1 >= 0 && y-1 >= 0 {
-		sw += world[x-1][y-1]
-	}
-	if y-1 >= 0 {
-		sw += world[x][y-1]
-	}
-	if x+1 < SIZE && y-1 >= 0 {
-		sw += world[x+1][y-1]
-	}
-	if x-1 >= 0 {
-		sw += world[x-1][y]
-	}
-	if x+1 < SIZE {
-		sw += world[x+1][y]
-	}
-	if x-1 >= 0 && y+1 < SIZE {
-		sw += world[x-1][y+1]
-	}
-	if y+1 < SIZE {
-		sw += world[x][y+1]
-	}
-	if x+1 < SIZE && y+1 < SIZE {
-		sw += world[x+1][y+1]
-	}
-
-	if sw < 2 {
-		shadowWorld[x][y] = 0
-	} else if sw == 3 {
-		shadowWorld[x][y] = 1
-	} else if sw > 3 {
-		shadowWorld[x][y] = 0
-	} else {
-		shadowWorld[x][y] = world[x][y]
-	}
+	screen.Show()
 }
